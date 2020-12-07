ccc09b843229aaacfd085a40149736f3910fa8d6
KyraStyl <32462777+KyraStyl@users.noreply.github.com>
2020-12-07 11:08:02 +0200

    class point extends Serializable

--- code/src/main/scala/Point.scala
+++ code/src/main/scala/Point.scala
@@ -1,6 +1,11 @@
-class Point (var line: String){
+class Point (var line: String) extends Serializable{
 
   private val cols = line.split(",").map(_.trim())
   val values = cols.toList
+  
+  override def toString: String = {
+    val str="Element : "+values.toString()+""
+    str
+  }
 
 }
